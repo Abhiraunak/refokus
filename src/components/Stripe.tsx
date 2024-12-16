166e322d90e65463a4ff9e58f319ab591cdac14a
Abhishek <abhishekkp1721@hotmail.com>
2024-12-16 15:01:12 +0530

    adding the stripes

--- src/components/Stripe.tsx
+++ src/components/Stripe.tsx
@@ -1,7 +1,10 @@
 
-function Stripe() {
+function Stripe({val}) {
   return (
-    <div>Stripe</div>
+    <div className="w-[16.66%] px-6 py-3 border-t-[1px] border-b-[1px] border-r-[1px] border-zinc-600 flex justify-between items-center">
+      <img src={val.url} alt="images"/>
+      <span className="font-semibold">{val.number}</span>
+    </div>
   )
 }
 
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -2,7 +2,7 @@
 function Work() {
   return (
     <div className="w-full">
-        <div className="max-w-screen-xl mx-auto text-center">
+        <div className="relative max-w-screen-xl mx-auto text-center">
             <h1 className="text-[30vw] leading-none font-medium tracking-tight select-none">work</h1>
         </div>
     </div>
